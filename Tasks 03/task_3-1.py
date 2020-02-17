3ad69e044523d190d3d47f05bfdfdac8f709a613
maxn-csi <maksimnaydin@coherentsolutions.com>
2020-02-17 18:57:12 +0300

    First task complete

--- Tasks 03/task_3-1.py
+++ Tasks 03/task_3-1.py
@@ -13,7 +13,46 @@
 # Два цвета называются совместимыми, если их яркости различаются больше, чем на 128.
 # Добавьте в класс Color метод для проверки совместимости двух цветов.
 
-class Color:
-    pass
+# Class definition -------------------------------------------------------
 
-red = Color()
+class Color():
+    def __init__(self, r, g, b):
+        def clamp(x):
+            return max(min(x, 255), 0)
+        self.r = clamp(r)
+        self.g = clamp(g)
+        self.b = clamp(b)
+        self.luminance()
+        
+    def red(self):
+        return self.r
+
+    def green(self):
+        return self.g
+
+    def blue(self):
+        return self.b
+
+    def cout(self):
+        return "R=" + str(self.r) + " G=" + str(self.g) + " B=" + str(self.b)
+
+    def luminance(self):
+        self.y = 0.299 * self.r + 0.587 * self.g + 0.114 * self.b
+        return self.y
+
+    def compare(self, color2):
+        return abs(self.luminance() - color2.luminance()) > 128
+
+# Main -------------------------------------------------------
+
+def main():
+    newColor = Color(-100, 128, 300)
+    newColor2 = Color(0, 255, 255)
+
+    # Output
+    print(newColor.cout())  # строковоe представлениe цвета
+    print(newColor.red())   # метод для получения цветового компонента объекта в виде числа
+    print(newColor.y)       # метод для вычисления яркости
+    print(newColor.compare(newColor2))  # метод для проверки совместимости двух цветов
+
+main()
